f680075167b3b120a7d67274099a951410ea2050
Leon <yanglyu.leon.7@gmail.com>
2021-03-07 10:26:30 -0800

    refactor

--- app/src/main/java/com/an7one/example/effectivejava/items/item28/counterexample/Demo.java
+++ app/src/main/java/com/an7one/example/effectivejava/items/item28/counterexample/Demo.java
@@ -1,19 +1,22 @@
 package com.an7one.example.effectivejava.items.item28.counterexample;
 
+import com.an7one.example.effectivejava.util.Constant;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class Demo {
-    public void demo(){
+    public void demo() {
         Object[] objectArray = new Long[1];
         // expected exception: ArrayStoreException
-        // objectArray[0] = "I do not fit in";
+//         objectArray[0] = "I do not fit in";
 
         // Will not compile
-        // List<Object> list = new ArrayList<Long>();
-        // list.add("I do not fit it");
+//         List<Object> list = new ArrayList<Long>();
+//         list.add("I do not fit it");
 
-        // Counter example: why is generic array creation illegal
+        // Counter example: why is generic array creation illegal?
 /*        List<String>[] stringLists = new List<String>[1];
         List<Integer> intList = List.of(42);
         Object[] objects = stringLists;
